1b00df8a61acc95b4a23e00ce6f37da4f1dd8c57
Audart Lucas <lucasaudart@gmail.com>
2024-07-29 13:45:43 +0200

    feat(app): add musicbrainz connection

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,14 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { debounce } from "./utils/debounce";
+import { IRelease } from "./types/musicbrainz.types";
+
+const musicApiHeader = {
+  Accept: "application/json",
+  "User-Agent": "ReactCompiler/0.1 ( lucasaudart@gmail.com )",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [search, setSearch] = useState("");
+  const [musicList, setMusicList] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (search !== "") {
+      fetch(
+        `http://musicbrainz.org/ws/2/release/?query=release:${search}&fmt=json`,
+        {
+          headers: musicApiHeader,
+        }
+      )
+        .then((response) => response.json())
+        .then((data: { releases: IRelease[] }) => {
+          data.releases.forEach((release) => {
+            fetch(`https://coverartarchive.org/release/${release.id}`)
+              .then((response) => response.json())
+              .then((data) => {
+                if (data.images[0]) {
+                  setMusicList((oldData) => [
+                    ...oldData,
+                    data.images[0].thumbnails.small,
+                  ]);
+                }
+              });
+          });
+        });
+    }
+  }, [search]);
+
+  const onChange = debounce(
+    (e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value),
+    500
+  );
 
   return (
     <>
       <div>
+        <input type="text" onChange={onChange} />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,19 +58,21 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>{musicList.length}</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <ul>
+          {musicList.map((music) => (
+            <li>
+              <img src={music} />
+            </li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

bf61cb32d0961852e1829ac5ee70263f3bb538c4
Audart Lucas <48870153+Slocaly@users.noreply.github.com>
2024-08-31 19:02:12 +0200

    feat(app): enhance the compil creation form (#12)

---
+++ apps/app/src/api/getAlbums.ts
@@ -0,0 +1,19 @@
+import { IRelease } from "../types/musicbrainz.types";
+
+const musicApiHeader = {
+  Accept: "application/json",
+  "User-Agent": "ReactCompiler/0.1 ( lucasaudart@gmail.com )",
+};
+
+export const getAlbums = async (search: string) => {
+  return await fetch(
+    `http://musicbrainz.org/ws/2/release/?query=artist:${search}&fmt=json`,
+    {
+      headers: musicApiHeader,
+    }
+  )
+    .then((response) => response.json())
+    .then((data: { releases: IRelease[] }) => {
+      return data.releases;
+    });
+};
---
+++ apps/app/src/components/Input.tsx
@@ -0,0 +1,19 @@
+import React, { forwardRef, InputHTMLAttributes } from "react";
+import "../styles/input.css";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  endSlot?: React.ReactNode;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ endSlot, ...props }, ref) => {
+    return (
+      <div>
+        <input ref={ref} {...props} />
+        {endSlot && <span>{endSlot}</span>}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";

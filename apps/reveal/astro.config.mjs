cbb345d1ccae9f378e5cc35d921249915dffb25b
CruuzAzul <alves.mckl@gmail.com>
2024-09-17 21:14:31 +0200

    chore: setup default theme + sandpack

--- apps/reveal/astro.config.mjs
+++ apps/reveal/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://jsulpis.github.io",
+	integrations: [react()],
 	base: "/",
 });
---
+++ apps/reveal/src/components/CodeEditor/index.tsx
@@ -0,0 +1,21 @@
+import { Sandpack, type SandpackProps } from "@codesandbox/sandpack-react";
+import "./styles.scss";
+
+export function CodeEditor(props: SandpackProps) {
+	const firstFile = Object.keys(props?.files || {})[0];
+
+	const options: SandpackProps["options"] = {
+		...props.options,
+		activeFile: firstFile,
+		showRefreshButton: false,
+		showTabs: true,
+		closableTabs: true,
+		showInlineErrors: true,
+	};
+
+	return (
+		<>
+			<Sandpack template="react" theme="light" {...props} options={options} />
+		</>
+	);
+}

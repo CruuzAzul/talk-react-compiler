a9edbefa5d8f7c9b931436b31cacd65ee2a03a62
Audart Lucas <lucasaudart@gmail.com>
2024-10-05 12:58:39 +0200

    feat(app): add text color from theme

--- apps/app/src/Root.tsx
+++ apps/app/src/Root.tsx
@@ -4,6 +4,8 @@ import { useThemeContext } from "./utils/useTheme";
 import { useAccount } from "./utils/useAccount";
 import { Devtools } from "./components/Devtools";
 import { useShowRerender } from "./utils/useShowRerender";
+import { contrast } from "./utils/contract";
+import { hexToRgb } from "./utils/hexToRgb";
 
 export const Root = () => {
   const { theme } = useThemeContext();
@@ -17,8 +19,19 @@ export const Root = () => {
     return <Navigate to="/sign-in" />;
   }
 
+  const rgb = hexToRgb(theme) ?? [255, 255, 255];
+  const textColor = contrast(rgb, [255, 255, 255]) > 4.5 ? "white" : "black";
+
   return (
-    <main style={{ backgroundColor: theme }} data-rerendered={showRerender}>
+    <main
+      style={
+        {
+          backgroundColor: theme,
+          "--color-text": textColor,
+        } as React.CSSProperties
+      }
+      data-rerendered={showRerender}
+    >
       {!isSignInPage && <Navbar />}
       <Outlet />
       <Devtools />
---
+++ apps/app/src/utils/contract.ts
@@ -0,0 +1,21 @@
+const RED = 0.2126;
+const GREEN = 0.7152;
+const BLUE = 0.0722;
+
+const GAMMA = 2.4;
+
+function luminance(r: number, g: number, b: number) {
+  const a = [r, g, b].map((v) => {
+    v /= 255;
+    return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, GAMMA);
+  });
+  return a[0] * RED + a[1] * GREEN + a[2] * BLUE;
+}
+
+export const contrast = (rgb1: [number, number, number], rgb2: [number, number, number]) => {
+  const lum1 = luminance(...rgb1);
+  const lum2 = luminance(...rgb2);
+  const brightest = Math.max(lum1, lum2);
+  const darkest = Math.min(lum1, lum2);
+  return (brightest + 0.05) / (darkest + 0.05);
+};

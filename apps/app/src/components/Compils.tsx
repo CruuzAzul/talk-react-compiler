c98e9264d9d26d0fdb4912681469613e218e5b8a
Audart Lucas <lucasaudart@gmail.com>
2024-10-05 12:33:32 +0200

    feat(app): remove filtering

--- apps/app/src/components/Compils.tsx
+++ apps/app/src/components/Compils.tsx
@@ -14,18 +14,6 @@ interface CompilsProps {
 export const Compils = ({ compilQueries, title }: CompilsProps) => {
   const { theme } = useThemeContext();
   const [compils, setCompils] = useState<Compil[] | null>(null);
-  const [search, setSearch] = useState("");
-
-  const filteredCompils =
-    compils?.filter(
-      (compil) =>
-        compil.name.toLowerCase().includes(search.toLowerCase()) ||
-        compil.album.some(
-          (album) =>
-            album.name.toLowerCase().includes(search.toLowerCase()) ||
-            album.artist.toLowerCase().includes(search.toLowerCase())
-        )
-    ) ?? [];
 
   const fetchCompils = async () => {
     const compils = await getCompils(compilQueries);
@@ -43,15 +31,8 @@ export const Compils = ({ compilQueries, title }: CompilsProps) => {
   return (
     <section className="compils-page" style={{ backgroundColor: theme }}>
       <h1 className="main-title">{title}</h1>
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Rechercher par titre ou artiste"
-        className="search-input"
-      />
       <ul className="compil-list">
-        {filteredCompils.map((compil) => (
+        {compils.map((compil) => (
           <li key={compil.$id}>
             <CompilCard compil={compil} />
           </li>
